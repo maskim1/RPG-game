caf61340dea244031d4f8ed3daa0b27dc3f7adf0
student_47 <student_47@DESKTOP-ROQAAM0>
2022-10-13 15:05:51 +0900

    초기 커밋.

---
+++ main.c
@@ -0,0 +1,36 @@
+
+#include "game.h"
+
+int main() {
+	srand(time(NULL));
+
+	while (1) {
+		print_status(player_hp, player_att, player_def,
+			monster_hp, monster_att, monster_def);
+
+		int choice = print_menu();
+
+		if (choice == 1) attack();
+		else if (choice == 2) defense();
+		else if (choice == 3) {
+			printf("Bye bye!\n");
+			break;
+		}
+		else {
+			printf("Wrong input\n");
+			continue;
+		}
+
+		if (player_hp <= 0) {
+			printf("You loose.\n");
+			break;
+		}
+		else if (monster_hp <= 0) {
+			printf("You won.\n");
+			break;
+		}
+	}
+
+	return 0;
+}
+
